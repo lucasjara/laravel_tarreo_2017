0cb1e5fa7864c6df62c3f11d00985696a34dacd2
lucasjara <lucasjaraespinoza@gmail.com>
2017-05-04 00:48:15 -0300

    Change table
    Bug Tables Fixed

--- app/Score.php
+++ app/Score.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+
+    public function users()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function categories()
+    {
+        return $this->belongsTo('App\Category');
+    }
 }
